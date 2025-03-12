ba49db968f80ee136d95b423deb754cb291f7206
azuredav <davenazure2004@gmail.com>
2025-03-12 23:13:19 +0800

    MLBA

---
+++ Py/Math.py
@@ -0,0 +1,21 @@
+#
+# Math
+#
+
+# 1. Input
+x1 = input("Type x1 ")
+x2 = input("Type x2 ")
+
+# 2. Process
+sum = int(x1) + int(x2)
+
+# 3. Output 
+print (f"sum: {sum}")
+
+min(10,20,30)
+max(10,20,30)
+
+import numpy as np
+np.mean([10,20,30])
+np.var([10,20,30])
+np.std([10,20,30])
